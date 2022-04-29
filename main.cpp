01fc64787c548e35384058bbf42fac5300ffe790
tomaszciula <tomciuns@gmail.com>
2022-04-29 15:54:02 +0200

    logger fixed, import logger.cpp to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "pch.h"
+#include "logger.cpp"
 
 uint8_t hammingDistance(uint8_t n1, uint8_t n2);
 void createFile1(const std::string name, const int count, const char value);
@@ -6,6 +7,11 @@ void createFile1(const std::string name, const int count, const char value);
 int main(int argc, char *argv[])
 {
     std::cout << "BER Calc v1.0" << std::endl;
+
+    openLog("log.log");
+    saveLog("Program uruchomiono poprawnie");
+    closeLog();
+
     std::cout << "argc =  : " << argc << std::endl;
     int iter = 0;
     for (iter = 0; iter < argc; iter++)
@@ -13,7 +19,7 @@ int main(int argc, char *argv[])
         std::cout << "argv[" << iter << "] =" << argv[iter] << std::endl;
     }
 
-    std::cout << (int)hammingDistance(0xFF, 0x11) << std::endl;
+    std::cout << (int)hammingDistance(0xFF, 0x01) << std::endl;
     createFile1("test1_file1.bin", 100, 0x55);
     return 0;
 }

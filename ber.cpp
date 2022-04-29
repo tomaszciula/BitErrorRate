c4f047a32ba4d02e6e43acb098208a67c8a8a387
tomaszciula <tomciuns@gmail.com>
2022-04-29 15:20:31 +0200

    .

--- ber.cpp
+++ ber.cpp
@@ -1,7 +1,13 @@
-#include <iostream>
+#include "pch.h"
 
-int main()
+int main(int argc, char * argv[])
 {
-    std::cout << "Hello BER ";
+    std::cout << "BER Calc v1.0" << std::endl;
+    std::cout << "argc =  : " << argc << std::endl; 
+    int iter = 0;
+    for (iter = 0; iter < argc; iter++) {
+        std::cout << "argv[" << iter << "] =" << argv[iter] << std::endl;
+    }
+
     return 0;
 }
--- pch.h
+++ pch.h
@@ -1,2 +1,7 @@
+<<<<<<< HEAD
 ﻿//#pragma once
+=======
+
+#pragma once
+>>>>>>> 5598deb91f96393b1838ba9daaee06af34ecab0c
 #include <iostream>
